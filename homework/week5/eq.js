518e80fe183e5b462a432d3e8741428074108830
Voytsekhovskiy Vladimir <vvo@corp.muk.ua>
2015-12-04 17:08:11 +0200

    aqq

--- homework/week5/eq.js
+++ homework/week5/eq.js
@@ -1,11 +1,10 @@
-'use strict'
 
 function eq(a, b, c) {
 
-    if (validator(a) & validator(b) & validator(c)) {
-        if (a == 0 & b == 0 & c == 0 || (a == undefined & b == undefined & c == undefined)) {
+    if (validator(a) && validator(b) && validator(c)) {
+        if (a == 0 && b == 0 && c == 0) {
             alert("0=0! Input data!");
-        } else if (a == 0 & b == 0 & c != 0 || (a == undefined & b == undefined)) {
+        } else if (a == 0 && b == 0 && c != 0 || (a == undefined && b == undefined)) {
             alert("Invalid input data! Check a and b!");
         } else {
             var d, x1, x2;
@@ -17,13 +16,10 @@ function eq(a, b, c) {
                 if (d > 0) {
                     x1 = (-b + Math.sqrt(d)) / (2 * a);
                     x2 = (-b - Math.sqrt(d)) / (2 * a);
-                } else if (d == 0) {
-                    x1 = -b / (2 * a);
-                    x2 = x1;
                 } else if (d < 0) {
                     var negativeSqr = Math.sqrt(Math.abs(d)) / (2 * a);
-                    x1 = -b / (2 * a) + " + " + (parseFloat(negativeSqr / (2 * a)) + "i");
-                    x2 = -b / (2 * a) + " - " + (parseFloat(negativeSqr / (2 * a)) + "i");
+                    x1 = -b / (2 * a) + " + " + negativeSqr / (2 * a) + "i";
+                    x2 = -b / (2 * a) + " - " + negativeSqr / (2 * a) + "i";
                 }
             }
             console.log("x1 = " + x1);
@@ -34,10 +30,6 @@ function eq(a, b, c) {
     }
 }
 function validator(a) {
-    if (isNaN(a)) {
-        return false;
-    } else if (!isFinite(a)) {
-        return false;
-    }
-    return true;
+    return !(!isFinite(a) || a == null || Math.abs(a) < Number.MIN_VALUE || a > Number.MAX_VALUE);
+
 }
